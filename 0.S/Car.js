dfa565ceff2c8c875db9c5e51fdca37810bf0ea3
Besart Elezi <besart.elezi@hotmail.com>
2022-08-01 10:47:41 +0200

    created Car class

--- 0.S/Car.js
+++ 0.S/Car.js
@@ -3,9 +3,44 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Car = void 0;
 var Car = /** @class */ (function () {
     function Car(_musicPlayer, _engine) {
+        this.FUEL_MILEAGE = 10;
+        this._miles = 0;
         this._musicPlayer = _musicPlayer;
         this._engine = _engine;
     }
+    Object.defineProperty(Car.prototype, "miles", {
+        get: function () {
+            return this._miles;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Car.prototype, "engine", {
+        get: function () {
+            return this._engine;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Car.prototype, "musicplayer", {
+        get: function () {
+            return this._musicPlayer;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Car.prototype.drive = function () {
+        if (this._engine.engineStatus === false || this._engine.fuel <= 0) {
+            //what I am doing here is a good principle called "failing early"
+            // If you have some conditions you need to check, that will exclude most of the code in your function check that first
+            // This prevents your "happy path" of code to be deeply indented.
+            return;
+        }
+        //removes fuel from engine
+        this._engine.useFuel();
+        //FUEL_MILEAGE needs to increase
+        this._miles += this.FUEL_MILEAGE;
+    };
     return Car;
 }());
 exports.Car = Car;
--- 0.S/Car.ts
+++ 0.S/Car.ts
@@ -1,11 +1,42 @@
 import {MusicPlayer} from "./MusicPlayer";
 import {Engine} from "./Engine";
 export class Car {
+
+    //adding other classes as properties of this class
     private _musicPlayer : MusicPlayer;
     private _engine : Engine;
+    private readonly FUEL_MILEAGE: number = 10;
+    private _miles : number = 0;
 
     constructor(_musicPlayer : MusicPlayer, _engine : Engine) {
         this._musicPlayer = _musicPlayer;
         this._engine = _engine;
     }
+
+    get miles(): number {
+        return this._miles;
+    }
+
+    get engine(): Engine {
+        return this._engine;
+    }
+
+    get musicplayer(): MusicPlayer {
+        return this._musicPlayer;
+    }
+
+    drive() {
+        if(this._engine.engineStatus === false || this._engine.fuel <= 0) {
+            //what I am doing here is a good principle called "failing early"
+            // If you have some conditions you need to check, that will exclude most of the code in your function check that first
+            // This prevents your "happy path" of code to be deeply indented.
+            return;
+        }
+        //removes fuel from engine
+        this._engine.useFuel();
+        //FUEL_MILEAGE needs to increase
+        this._miles += this.FUEL_MILEAGE;
+    }
+
+
 }
