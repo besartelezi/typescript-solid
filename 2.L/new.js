8c0990802af7c27d87c0cb26686655ab7a55ee4d
Besart Elezi <besart.elezi@hotmail.com>
2022-08-01 16:42:43 +0200

    added new main ts file

---
+++ 2.L/new.js
@@ -0,0 +1,22 @@
+var cart = new shoppingBasket();
+cart.addProduct(new Product('Chair', 25, new Discount("fixed", 10)));
+//cart.addProduct(new Product('Chair', 25, new Discount("fixed", -10)));
+cart.addProduct(new Product('Table', 50, new Discount("variable", 25)));
+cart.addProduct(new Product('Bed', 100, new Discount("none")));
+var tableElement = document.querySelector('#cart tbody');
+cart.products.forEach(function (product) {
+    var tr = document.createElement('tr');
+    var td = document.createElement('td');
+    td.innerText = product.name;
+    tr.appendChild(td);
+    td = document.createElement('td');
+    td.innerText = product.originalPrice.toFixed(2) + " €";
+    tr.appendChild(td);
+    td = document.createElement('td');
+    td.innerText = product.calculatePrice().toFixed(2) + " €";
+    tr.appendChild(td);
+    td = document.createElement('td');
+    td.innerText = product.showCalculation();
+    tr.appendChild(td);
+    tableElement.appendChild(tr);
+});
---
+++ 2.L/new.ts
@@ -0,0 +1,28 @@
+let cart = new shoppingBasket();
+cart.addProduct(new Product('Chair', 25, new Discount("fixed", 10)));
+//cart.addProduct(new Product('Chair', 25, new Discount("fixed", -10)));
+cart.addProduct(new Product('Table', 50, new Discount("variable", 25)));
+cart.addProduct(new Product('Bed', 100, new Discount("none")));
+
+const tableElement = document.querySelector('#cart tbody');
+cart.products.forEach((product) => {
+    let tr = document.createElement('tr');
+
+    let td = document.createElement('td');
+    td.innerText = product.name;
+    tr.appendChild(td);
+
+    td = document.createElement('td');
+    td.innerText = product.originalPrice.toFixed(2) + " €";
+    tr.appendChild(td);
+
+    td = document.createElement('td');
+    td.innerText = product.calculatePrice().toFixed(2) + " €";
+    tr.appendChild(td);
+
+    td = document.createElement('td');
+    td.innerText = product.showCalculation();
+    tr.appendChild(td);
+
+    tableElement.appendChild(tr);
+});
